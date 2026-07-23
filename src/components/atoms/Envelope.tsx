f4116d28385f59cf6d8a2ffad77aaa125a4820da
DominikFrackowiak <rorschach@op.pl>
2026-07-23 23:09:12 +0200

    refactor: rebuild UI with Astro components

--- src/components/atoms/Envelope.tsx
+++
@@ -1,9 +0,0 @@
-import { PiEnvelopeThin } from 'react-icons/pi'
-
-export default function Envelope({ email }: { email: string; text: string }) {
-	return (
-		<a href={`mailto:${email}`} className='text-white font-open-sans'>
-			<PiEnvelopeThin className='text-2xl text-white' />
-		</a>
-	)
-}
